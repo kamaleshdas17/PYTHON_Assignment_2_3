6288dbb34387b3c0176336392078ca3b2bde95b1
kamaleshdas17 <37649215+kamaleshdas17@users.noreply.github.com>
2018-04-09 18:16:52 -0700

    Add files via upload

---
+++ PYTHON_Assignment_2_3.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program to reverse a word after accepting the input from the user.
+Sample Output:
+Input word: AcadGild
+Output: dilGdacA
+
+"""
+
+input=input("Please enter any word: ")
+print ('Output: ',input[::-1])
